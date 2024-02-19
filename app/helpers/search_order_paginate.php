8c68ac3556751a4de5f1b5a1d04b3fbc468fa870
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-19 20:20:04 +0300

    0.47.2

--- app/helpers/search_order_paginate.php
+++ app/helpers/search_order_paginate.php
@@ -3,7 +3,7 @@
 
 function search_order_paginate($product_moves, $request) {
     multi_fields_search($product_moves, $request->search_target)->
-    orderBy('date');
+    orderBy('created_at');
     paginate($product_moves,
         per_page: $request->session()->get('per_page') ?? 10,
         current_page: $request->current_page ?? 1,
