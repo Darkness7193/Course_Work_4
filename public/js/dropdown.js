251de560c3164665896034cda80b0745707dafc5
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-24 02:23:09 +0300

    0.70.0

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,7 +2,6 @@ function toggle_dropdown_content(drop_btn) {
     let dropdown_content = drop_btn.parentNode.getElementsByClassName('dropdown-content')[0]
     if (['', 'none'].includes(dropdown_content.style.display)) {
         dropdown_content.style.display = 'block'
-
     } else {
         dropdown_content.style.display = 'none'
     }
@@ -10,9 +9,7 @@ function toggle_dropdown_content(drop_btn) {
 
 
 function dropdown_focus_out (event) {
-    if (event.target.matches('.drop-btn')) {
-        return
-    }
+    if (event.target.matches('.drop-btn')) { return }
 
     let dropdowns = document.getElementsByClassName("dropdown-content");
     for (let i = 0; i < dropdowns.length; i++) {
