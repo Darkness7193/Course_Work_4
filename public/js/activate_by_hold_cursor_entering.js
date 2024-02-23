812448e31d8efb381a11534e7b03ae331ca85ca3
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-24 01:21:43 +0300

    0.69.1 some refactoring of activate_by_hold_cursor_entering.js

--- public/js/activate_by_hold_cursor_entering.js
+++ public/js/activate_by_hold_cursor_entering.js
@@ -12,26 +12,26 @@ document.addEventListener('mouseup', function(event) {
     is_mouse_down = false
 }, true)
 
+function disable_context_menu(event) { event.preventDefault() }
 
-function activate_by_hold_cursor_entering(elements) {
+function activate_by_hold_cursor_entering(element) {
 
-    function disable_context_menu(event) { event.preventDefault() }
-
-    for (let i=0, n=elements.length; i<n; i++) {
-        elements[i].addEventListener("mouseenter", (event) => {
-            if (is_mouse_down) {
-                elements[i].click()
-                window.addEventListener(`contextmenu`, disable_context_menu)
-                document.addEventListener('mouseup', function activate_context_menu(event) {
-                    event.currentTarget.removeEventListener(event.type, activate_context_menu)
-                    msleep(200).then(()=>{
-                        window.removeEventListener(`contextmenu`, disable_context_menu)
-                    })
-                })
-            }
+    element.addEventListener("mouseenter", (event) => { if (is_mouse_down) {
+        element.click()
+        window.addEventListener(`contextmenu`, disable_context_menu)
+        document.addEventListener('mouseup', function activate_context_menu(event) {
+            event.currentTarget.removeEventListener(event.type, activate_context_menu)
+            msleep(200).then(()=>{
+                window.removeEventListener(`contextmenu`, disable_context_menu)
+            })
         })
-    }
+    } })
 }
 
 
-activate_by_hold_cursor_entering(document.getElementsByClassName('delete-btn'))
+let delete_btns = document.getElementsByClassName('delete-btn')
+for (let i=0, n=delete_btns.length; i<n; i++) {
+    activate_by_hold_cursor_entering(delete_btns[i])
+}
+
+
