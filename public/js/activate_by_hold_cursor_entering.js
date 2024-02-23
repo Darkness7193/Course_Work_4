449400773851f39ebdb05851882b2f6d0b3fee88
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-24 01:12:07 +0300

    0.69 bulk delete-btn activation by hold cursor now don't trigger context menu

--- public/js/activate_by_hold_cursor_entering.js
+++ public/js/activate_by_hold_cursor_entering.js
@@ -1,3 +1,6 @@
+import { msleep } from './helpers.js'
+
+
 var is_mouse_down = false
 
 document.addEventListener('mousedown', function(event) {
@@ -12,10 +15,19 @@ document.addEventListener('mouseup', function(event) {
 
 function activate_by_hold_cursor_entering(elements) {
 
+    function disable_context_menu(event) { event.preventDefault() }
+
     for (let i=0, n=elements.length; i<n; i++) {
         elements[i].addEventListener("mouseenter", (event) => {
             if (is_mouse_down) {
                 elements[i].click()
+                window.addEventListener(`contextmenu`, disable_context_menu)
+                document.addEventListener('mouseup', function activate_context_menu(event) {
+                    event.currentTarget.removeEventListener(event.type, activate_context_menu)
+                    msleep(200).then(()=>{
+                        window.removeEventListener(`contextmenu`, disable_context_menu)
+                    })
+                })
             }
         })
     }
