e3963fd15e4268c2d2b6b45928830bdea66e6538
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-24 15:35:05 +0300

    0.72.2

--- app/helpers/filter_order_paginate.php
+++ app/helpers/filter_order_paginate.php
@@ -5,8 +5,9 @@ include_once(app_path().'/helpers/paginate.php');
 
 
 function filter_order_paginate($product_moves, $request) {
-    where_some_field_like($product_moves, $request->search_target)->
-    orderBy('created_at');
+    where_some_field_like($product_moves, $request->search_target)
+        ->orderBy('created_at');
+
     paginate($product_moves,
         per_page: $request->session()->get('per_page') ?? 10,
         current_page: $request->current_page ?? 1,
