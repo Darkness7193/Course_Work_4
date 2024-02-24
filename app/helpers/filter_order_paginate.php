2e8033d156ec34dfade33e63762610a9a60705ab
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-24 15:36:28 +0300

    0.72.3

--- app/helpers/filter_order_paginate.php
+++ app/helpers/filter_order_paginate.php
@@ -8,10 +8,8 @@ function filter_order_paginate($product_moves, $request) {
     where_some_field_like($product_moves, $request->search_target)
         ->orderBy('created_at');
 
-    paginate($product_moves,
+    return paginate($product_moves,
         per_page: $request->session()->get('per_page') ?? 10,
         current_page: $request->current_page ?? 1,
     );
-
-    return $product_moves;
 }
