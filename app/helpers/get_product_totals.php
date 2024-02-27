16a5765ec5f6a39e4a82e9dd8acbbb66e8d93e7d
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-27 22:32:40 +0300

    0.84 update get_product_totals.php query

--- app/helpers/get_product_totals.php
+++ app/helpers/get_product_totals.php
@@ -5,7 +5,7 @@ include_once(app_path().'/helpers/paginate.php');
 use Illuminate\Support\Facades\DB;
 
 
-function query_totals() {
+function query_totals_2() {
     $purchases = "
     select *
     from product_moves
@@ -34,6 +34,7 @@ function query_totals() {
         inner join ($sales) as s
             on p.storage_id = s.storage_id
             or p.product_id = s.product_id
+
     group by
         p.storage_id, p.product_id
     order by
@@ -45,7 +46,31 @@ function query_totals() {
 
 
 function get_product_totals($request) {
-    $totals = DB::select(query_totals());
+    $totals = DB::select("
+    select
+        (select name from storages where id = storage_id) as storage_name,
+        (select name from products where id = product_id) as product_name,
+        sum(case product_move_type
+            when 'purchasing'  then  quantity
+            when 'selling'     then -quantity
+            when 'liquidating' then -quantity
+            when 'inventory'   then  quantity
+            when 'transfering' then -quantity
+            end) as total_quantity,
+        sum(case product_move_type
+            when 'purchasing'  then  quantity*price
+            when 'selling'     then -quantity*price
+            when 'liquidating' then -quantity*price
+            when 'inventory'   then  quantity*price
+            when 'transfering' then -quantity*price
+            end) as income,
+        sum(if(product_move_type = 'purchasing', price, 0))          as total_purchases_price,
+        sum(if(product_move_type = 'purchasing', price*quantity, 0)) as total_purchases_quantity,
+        sum(if(product_move_type = 'selling', price, 0))             as total_sales_price,
+        sum(if(product_move_type = 'selling', price*quantity, 0))    as total_sales_quantity
+    from product_moves
+    group by storage_id, product_id
+    ");
 
     return paginate_array($totals,
         per_page: $request->session()->get('per_page') ?? 10,
