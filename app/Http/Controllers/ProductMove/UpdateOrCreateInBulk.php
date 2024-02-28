417e15dc12b5be7ed01063582cd927d2597527de
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-28 23:57:48 +0300

    0.90 UpdateOrCreateInBulk.php again guarded against not full filled new rows. I should change this if's on the validate

--- app/Http/Controllers/ProductMove/UpdateOrCreateInBulk.php
+++ app/Http/Controllers/ProductMove/UpdateOrCreateInBulk.php
@@ -12,13 +12,18 @@ class UpdateOrCreateInBulk extends Controller
 {
     public function __invoke(Request $request): void
     {
-        foreach ($request->updated_rows as $row_id => $updated_cells) {
+        $fillable_count = count((new ProductMove)->getFillable());
+        foreach ($request->updated_rows as $row_id => $updated_cells)
+        {
             $exist_purchase = ProductMove::find($row_id);
-
             if ($exist_purchase) {
                 $exist_purchase->update($updated_cells);
             } else {
-                ProductMove::create(array_merge($request->no_view_fields, $updated_cells));
+                $new_row = array_merge($request->no_view_fields, $updated_cells);
+                $is_all_fields_filled = count($new_row) === $fillable_count;
+                if ($is_all_fields_filled) {
+                    ProductMove::create($new_row);
+                }
             }
         }
     }
