bc4834a33cb59cce2fed0da7f5fa13ef13186034
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-02-29 20:23:47 +0300

    0.105.6

--- resources/views/pages/purchases-crud.blade.php
+++ resources/views/pages/purchases-crud.blade.php
@@ -8,7 +8,7 @@
 
 
 <!-- imports: -->
-<script src="{{ asset('js/auto_new_tr.js') }}" type="module"></script>
+    <script src="{{ asset('js/auto_new_tr.js') }}" type="module"></script>
     <link rel="stylesheet" href="{{ asset('css/crud-table.css') }}">
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
