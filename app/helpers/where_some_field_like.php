d0d35a83754d610ba58e314d7f7d40277f3bc408
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-02 21:14:38 +0300

    0.121 where_field_like() encapsulating

--- app/helpers/where_some_field_like.php
+++ app/helpers/where_some_field_like.php
@@ -5,15 +5,22 @@ function is_foreing_id($field) { return str_contains($field, 'id'); }
 function get_foreign_name($foreign_id) { return substr($foreign_id, 0, -3); }
 
 
-function where_some_field_like(&$query, $target, $search_fields=null) {
-    if (empty($target)) { return $query; }
+function where_field_like(&$rows, $target, $search_field) {
+    if (empty($target)) { return $rows; }
 
-    return $query->where( function($query) use($target, $search_fields) {
-        foreach ($search_fields as $field)
-        {
-            $query = is_foreing_id($field)
-                ? $query->orWhereRelation(get_foreign_name($field), 'name', 'like', "%$target%")
-                : $query->orWhere($field, 'like', "%$target%");
+    return is_foreing_id($search_field)
+        ? $rows->orWhereRelation(get_foreign_name($search_field), 'name', 'like', "%$target%")
+        : $rows->orWhere($search_field, 'like', "%$target%");
+}
+
+
+function where_some_field_like(&$rows, $target, $search_fields) {
+    if (empty($target)) { return $rows; }
+
+    return $rows->where( function($rows) use($target, $search_fields)
+    {
+        foreach ($search_fields as $field) {
+            where_field_like($rows, $target, $field);
         }
     });
 }
