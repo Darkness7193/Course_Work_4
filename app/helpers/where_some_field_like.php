dbc3bbca74311ca5c96ee5874fea92ea35193e8d
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-02 19:11:07 +0300

    0.120 where_some_field_like()

--- app/helpers/where_some_field_like.php
+++ app/helpers/where_some_field_like.php
@@ -1,20 +1,19 @@
 <?php
 
 
+function is_foreing_id($field) { return str_contains($field, 'id'); }
+function get_foreign_name($foreign_id) { return substr($foreign_id, 0, -3); }
+
+
 function where_some_field_like(&$query, $target, $search_fields=null) {
     if (empty($target)) { return $query; }
-    $target = "%$target%";
 
     return $query->where( function($query) use($target, $search_fields) {
-        foreach ($search_fields ?? array_keys($query->first()->toArray()) as $field) {
-            $is_foreing_id = str_contains($field, 'id');
-
-            if ($is_foreing_id) {
-                $foreign_record = substr($field, 0, -3);
-                $query = $query->orWhereRelation($foreign_record, 'name', 'like', $target);
-            } else {
-                $query = $query->orWhere($field, 'like', $target);
-            }
+        foreach ($search_fields as $field)
+        {
+            $query = is_foreing_id($field)
+                ? $query->orWhereRelation(get_foreign_name($field), 'name', 'like', "%$target%")
+                : $query->orWhere($field, 'like', "%$target%");
         }
     });
 }
