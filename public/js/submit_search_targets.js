cb3a851bd402dc750f4a3eec98d8983fc4294c37
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-02 19:04:12 +0300

    0.119 submit_search_targets() and submit_empty_search_targets() renaming and refactoring

--- public/js/submit_search_targets.js
+++ public/js/submit_search_targets.js
@@ -1,27 +1,29 @@
 import { post } from './helpers.js'
 
 
-function submit_filter_targets() {
-    let targets = {'target': document.getElementsByClassName('search-input')[0].value}
+function submit_search_targets() {
+    let cols_targets = {}
     ;[...document.getElementsByClassName('advanced-search-input')].forEach((col_search_input)=>{
-        targets[col_search_input.dataset.viewField] = col_search_input.value
+        cols_targets[col_search_input.dataset.viewField] = col_search_input.value
     })
 
     return post(window.post_to_get_route_route, {
         'target_route': window.current_route,
-        'filter_targets': targets
+        'search_col_targets': cols_targets,
+        'search_cols_target': document.getElementsByClassName('search-input')[0].value
     })
 }
 
 
-function submit_empty_filter_targets() {
-    let targets = {'target': document.getElementsByClassName('search-input')[0].value}
+function submit_empty_search_targets() {
+    let cols_targets = {}
     ;[...document.getElementsByClassName('advanced-search-input')].forEach((col_search_input)=>{
-        targets[col_search_input.dataset.viewField] = col_search_input.value
+        cols_targets[col_search_input.dataset.viewField] = ''
     })
 
     return post(window.post_to_get_route_route, {
         'target_route': window.current_route,
-        'filter_targets': targets
+        'search_col_targets': cols_targets,
+        'search_cols_target': ''
     })
 }
