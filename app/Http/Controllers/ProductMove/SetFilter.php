424a816ce6d9389228642d001f55d09180209f55
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-04 01:59:44 +0300

    0.144 :1

--- app/Http/Controllers/ProductMove/SetFilter.php
+++ app/Http/Controllers/ProductMove/SetFilter.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\ProductMove;
 
-include_once(app_path().'/helpers/get_search_targets.php');
+include_once(app_path().'/helpers/get_form_data.php');
 
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+
+
+function get_search_targets($request, $postfix) {
+    $targets = get_form_data($request, $postfix);
+    $tablewise = $targets['tablewise'];
+    unset($targets['tablewise']);
+
+    return [
+        'tablewise' => $tablewise,
+        'fieldwise' => $targets
+    ];
+}
 
 
 class SetFilter extends Controller
@@ -14,7 +26,7 @@ class SetFilter extends Controller
         $is_un_search = $request->action === 'un_search';
 
         return to_route($request->target_route, [
-            'search_targets' => $is_un_search ? null : get_search_targets($request)
+            'search_targets' => $is_un_search ? null : get_search_targets($request, '_search_target')
         ]);
     }
 }
