69a25ff7d9e743c045e1bd6113a0d531848f43a2
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-03 21:08:02 +0300

    0.133 :2 replicate old behavior of search with fieldwise as separete from $targets new array

--- app/helpers/get_search_targets.php
+++ app/helpers/get_search_targets.php
@@ -2,14 +2,15 @@
 
 
 function get_search_targets($request) {
-    $targets = [];
+    $targets = ['tablewise' => $request->tablewise_search_target];
     $postfix_len = strlen('_search_target');
 
-    foreach ($request->all() as $maybe_search_field => $value) {
-        if (str_contains($maybe_search_field, '_search_target')) {
-            $search_field = substr($maybe_search_field, 0, -$postfix_len);
-            $targets[$search_field] = $value;
+    $fieldwise_targets = [];
+    foreach ($request->except('tablewise_search_target') as $maybe_target_field => $target) {
+        if (str_contains($maybe_target_field, '_search_target')) {
+            $target_field = substr($maybe_target_field, 0, -$postfix_len);
+            $fieldwise_targets[$target_field] = $target;
         }
     }
-    return $targets;
+    return array_merge($targets, $fieldwise_targets);
 }
