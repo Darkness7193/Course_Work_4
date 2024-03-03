4ca811075ed9125c0f8beb7be88730011a257727
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-03 22:41:16 +0300

    0.138 :1 advanced search -> fieldwise search

--- resources/views/table-tools/fieldwise-search-btn.blade.php
+++ resources/views/table-tools/fieldwise-search-btn.blade.php
@@ -1,19 +1,19 @@
 <!-- imports: -->
-    <link rel="stylesheet" href="{{ asset('css/table-tools/advanced-search-menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table-tools/fieldwise-search-menu.css') }}">
     <link rel="stylesheet" href="{{ asset('css/abstract/dropdown.css') }}">
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <script src="{{ asset('js/of_abstract/of_dropdown.js') }}" type="module"></script>
 
 
 <!-- f($view_fields, $headers): -->
-<div class="advanced-search-btn dropdown icon">
+<div class="fieldwise-search-btn dropdown icon">
     <button class="drop-btn icon" type="button" onclick="toggle_dropdown_content(this)"></button>
-    <div class="advanced-search-menu dropdown-content">
+    <div class="fieldwise-search-menu dropdown-content">
         <table>
             @foreach($view_fields as $rw => $view_field)
                 <tr>
                     <td>{{ $headers[$rw] }}:</td>
-                    <td><input class="advanced-search-input"
+                    <td><input class="fieldwise-search-input"
                         name="{{ $view_field }}_search_target"
                         type="text"
                         onfocus="this.select();"
--- resources/views/table-tools/search-bar.blade.php
+++ resources/views/table-tools/search-bar.blade.php
@@ -1,25 +1,27 @@
 <!-- imports: -->
-    <link rel="stylesheet" href="{{ asset('css/table-tools/search-bar.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+<link rel="stylesheet" href="{{ asset('css/table-tools/search-bar.css') }}">
+<link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
 
 <!-- f($search_target, $view_fields, $headers): -->
 <form class="search-bar vertical-arrange vertical-center"
-    method="post"
-    action="{{ route('product_moves.set_filter', ['target_route' => Route::current()->getName()]) }}"
+      method="post"
+      action="{{ route('product_moves.set_filter', ['target_route' => Route::current()->getName()]) }}"
 >   @csrf
 
-    <div class="default-input-wrapper"><button name="action" value="search"></button></div>
+    <div class="default-input-wrapper">
+        <button name="action" value="search"></button>
+    </div>
 
-    @include('table-tools.advanced-search-btn', ['view_fields' => $view_fields,'headers' => $headers])
+    @include('table-tools.fieldwise-search-btn', ['view_fields' => $view_fields,'headers' => $headers])
     <button class="icon anti-search-btn" type="submit" name="action" value="un_search"></button>
     <button class="icon search-btn" type="submit" name="action" value="search"></button>
 
     <input class="search-input"
-        name="tablewise_search_target"
-        type="text"
-        placeholder="Фильтр"
-        onfocus="this.select();"
-        autofocus
+           name="tablewise_search_target"
+           type="text"
+           placeholder="Фильтр"
+           onfocus="this.select();"
+           autofocus
     >
 </form>
