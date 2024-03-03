4740252cc38b0d65eea643c9439fad786af96711
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-03 22:18:18 +0300

    0.137 :1

--- app/functions/queries/filter_order_paginate.php
+++ app/functions/queries/filter_order_paginate.php
@@ -2,20 +2,8 @@
 
 include_once(app_path().'/helpers/eloquent/where_field_like.php');
 include_once(app_path().'/helpers/eloquent/where_some_field_like.php');
+include_once(app_path().'/helpers/eloquent/filter.php');
 include_once(app_path().'/helpers/eloquent/paginate.php');
-
-
-function filter($rows, $search_targets, $view_fields) {
-    if (empty($search_targets)) { return $rows; }
-
-    $rows = where_some_field_like($rows, $search_targets['tablewise'] ?? null, $view_fields);
-
-    foreach ($search_targets['fieldwise'] as $target_field => $search_target) {
-        $rows = where_field_like($rows, $search_target, $target_field);
-    }
-
-    return $rows;
-}
 
 
 function filter_order_paginate($product_moves, $view_fields, $request) {
---
+++ app/helpers/eloquent/filter.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function filter($rows, $search_targets, $view_fields) {
+    if (empty($search_targets)) { return $rows; }
+
+    $rows = where_some_field_like($rows, $search_targets['tablewise'] ?? null, $view_fields);
+
+    foreach ($search_targets['fieldwise'] as $target_field => $search_target) {
+        $rows = where_field_like($rows, $search_target, $target_field);
+    }
+
+    return $rows;
+}
