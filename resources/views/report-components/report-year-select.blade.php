2a95f9e2e873860a9ad03c73db5f0fd16f6f27e9
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-06 02:45:26 +0300

    0.156 :1

--- resources/views/report-components/report-year-select.blade.php
+++ resources/views/report-components/report-year-select.blade.php
@@ -4,11 +4,11 @@
 <form>
     <select style="width: 200px;" name="year_of_report" onchange="this.form.submit()">
         @foreach ($used_years as $used_year)
-            <option value="{{ $used_year }}">{{ $used_year }}</option>
+            <option value="{{ $used_year }}">{{ $used_year }} год</option>
         @endforeach
 
         <option selected="selected" hidden="hidden" value="{{ $year_of_report }}">
-            {{ $year_of_report }}
+            {{ $year_of_report }} год
         </option>
     </select>
 </form>
