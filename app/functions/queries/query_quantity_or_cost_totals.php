aadd7b50aadb6a34e9b0c4183e1e3c11f864160a
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-09 05:03:13 +0300

    0.162 :1

--- app/functions/queries/query_quantity_or_cost_totals.php
+++ app/functions/queries/query_quantity_or_cost_totals.php
@@ -26,25 +26,31 @@ function select_totals_by_months(&$query, $calculated_field) {
 }
 
 
-function query_quantity_or_cost_totals($request, $report_field_i, $report_storage_id, $year) {
-    $report_storage_id = $report_storage_id ?: Storage::first()->id;
-    $report_field = ['quantity', 'quantity*price'][$report_field_i];
-
-
-    $transfered = DB::table('product_moves')
+function transfered($report_storage_id, $report_field) {
+    return DB::table('product_moves')
         ->where('storage_id', '=', $report_storage_id)
         ->where('product_move_type', '=', 'transfering')
         ->groupBy('product_id')
-        ->select('product_id As transfered_product_id', DB::raw("Sum($report_field) As transfered_totals"));
+        ->select('product_id As transfered_product_id')
+        ->selectRaw("Sum($report_field) As transfered_totals");
+}
 
 
-    $ever = DB::table('product_moves')
+function ever($report_storage_id, $report_field) {
+    return DB::table('product_moves')
         ->where('storage_id', '=', $report_storage_id)
         ->where('product_move_type', '=', 'transfering')
         ->groupBy('product_id')
         ->select('product_id As ever_product_id')
         ->selectRaw("Sum(If(product_move_type In ('purchasing', 'inventory'), $report_field, -$report_field)) As ever_totals");
+}
 
+
+function query_quantity_or_cost_totals($request, $report_field_i, $report_storage_id, $year) {
+    $report_storage_id = $report_storage_id ?: Storage::first()->id;
+    $report_field = ['quantity', 'quantity*price'][$report_field_i];
+    $transfered = transfered($report_storage_id, $report_field);
+    $ever = ever($report_storage_id, $report_field);
 
     $totals = DB::table('product_moves as this')
         ->joinSub($transfered, 'transfered', on('this.product_id', '=', 'transfered_product_id'))
@@ -52,11 +58,10 @@ function query_quantity_or_cost_totals($request, $report_field_i, $report_storag
         ->where('this.storage_id', '=', $report_storage_id)
         ->where(DB::raw('year(date)'), '=', $year)
         ->groupBy('this.product_id')
-        ->select('ever_totals')
-        ->selectRaw("(Select name From products Where id = product_id) As product_name")
-        ->selectRaw("Sum(If(product_move_type IN ('purchasing', 'inventory'), $report_field, -$report_field)) As year_totals");
-    select_totals_by_months($totals, $report_field);
-
+        ->selectRaw("ever_totals,
+            (Select name From products Where id = product_id) As product_name,
+            Sum(If(product_move_type IN ('purchasing', 'inventory'), $report_field, -$report_field)) - transfered_totals As year_totals");
+        select_totals_by_months($totals, $report_field);
 
     return paginate($totals,
         per_page: $request->session()->get('per_page') ?? 10,
