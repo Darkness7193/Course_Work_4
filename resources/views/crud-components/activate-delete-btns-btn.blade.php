ddb3d9498c74be661b9a9a4a5571ae2fec71fc86
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-16 01:59:44 +0300

    0.173 fix bug with activate_delete_btns()

--- resources/views/crud-components/activate-delete-btns-btn.blade.php
+++ resources/views/crud-components/activate-delete-btns-btn.blade.php
@@ -1,14 +1,14 @@
 <script>
-    function activate_all_delete_btn() {
+    function activate_delete_btns() {
         ;[...document.getElementsByClassName('delete-btn')].forEach((delete_btn)=>{
             let img = delete_btn.getElementsByTagName('img')[0]
-            if (img.src === window.img_delete_off){ delete_btn.click() }
+            if (img.src === window.php_vars['img_delete_off']){ delete_btn.click() }
         })
     }
 </script>
 
 
 <!-- f(): -->
-<button type="button" class="btn" onclick="activate_all_delete_btn()">
+<button type="button" class="btn" onclick="activate_delete_btns()">
     <img class='btn-icon' src="{{ asset('images/trash-can-icon.jpg') }}"/>
 </button>
