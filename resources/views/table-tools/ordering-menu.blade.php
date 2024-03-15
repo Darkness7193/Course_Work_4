b501fc3709d3fda9db6fdaa812dc85677f88d38d
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-16 00:46:47 +0300

    0.172 :5

--- resources/views/table-tools/ordering-menu.blade.php
+++ resources/views/table-tools/ordering-menu.blade.php
@@ -20,23 +20,23 @@
                 @foreach(array_merge($view_fields, ['created_at', 'updated_at']) as $rw => $view_field)
                     <tr>
                         <td>{{ array_merge($headers, ['# по созданию', '# по изменению'])[$rw] }}:</td>
+                        <td>
+                            <button class="icon order-direction-btn"
+                                type="button"
+                                onclick="toggle_ordering_direction(this)">
+                            </button>
+                        </td>
+                        <td><input class="order-direction-input"
+                            hidden="hidden"
+                            name="{{ $view_field }}_order_direction"
+                            value="asc"
+                        ></td>
+
                         <td><input class="number-checkbox-input"
                             name="{{ $view_field }}_order_priority"
                             type="number"
                             readonly
                         ></td>
-
-                        <td>
-                            <button class="icon order-direction-btn"
-                                    type="button"
-                                    onclick="toggle_ordering_direction(this)">
-                            </button>
-                        </td>
-                        <td><input class="order-direction-input"
-                                   hidden="hidden"
-                                   name="{{ $view_field }}_order_direction"
-                                   value="asc"
-                            ></td>
                     </tr>
                 @endforeach
                 <tr>
