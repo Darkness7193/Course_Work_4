76303885a205c732b47b8d385a62aab1c0225a3e
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-23 18:51:57 +0300

    0.191 :4

--- app/Http/Controllers/ProductMove/SetFilter.php
+++ app/Http/Controllers/ProductMove/SetFilter.php
@@ -25,10 +25,8 @@ function get_search_targets($request, $postfix) {
 class SetFilter extends Controller
 {
     public function __invoke(Request $request){
-        $is_un_search = $request->action === 'un_search';
-
         return to_route($request->target_route, [
-            'search_targets' => $is_un_search ? null : get_search_targets($request, '_search_target')
+            'search_targets' => $request->action === 'un_search' ? null : get_search_targets($request, '_search_target')
         ]);
     }
 }
