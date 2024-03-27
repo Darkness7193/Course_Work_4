79588e59a84fed4964ac2252c9c071849987899f
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-27 16:46:06 +0300

    0.211 :1

--- public/js/of_crud-table/auto_price_insert.js
+++ public/js/of_crud-table/auto_price_insert.js
@@ -7,8 +7,8 @@ function auto_price_insert(product_select) {
     let purchase_price_input = parent_tr.getElementsByClassName('purchase-price-input')[0]
     let sale_price_input = parent_tr.getElementsByClassName('sale-price-input')[0]
 
-    if (purchase_price_input && !purchase_price_input.value) { purchase_price_input.value = selected_option.dataset.purchasePrice }
-    if (sale_price_input && !sale_price_input.value) { sale_price_input.value = selected_option.dataset.salePrice }
+    if (purchase_price_input) { purchase_price_input.value = selected_option.dataset.purchasePrice }
+    if (sale_price_input) { sale_price_input.value = selected_option.dataset.salePrice }
 }
 
 
