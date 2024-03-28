2c3f86079531d81f7fc049b563be9a5962583ca6
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-28 16:26:33 +0300

    0.214 Home now not so ugly. Out of dark reader

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,39 @@
 @extends('layouts.app')
 
+
 @section('content')
+<!-- imports: -->
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-
+                <div class="card-header">{{ __('Движения') }}</div>
+                <div class="card-body vertical-arrange">
+                    <button class="page-btn" onclick="window.location='{{ route('product_moves.purchases_crud') }}'"> Покупки </button>
+                    <button class="page-btn" onclick="window.location='{{ route('product_moves.sales_crud') }}'"> Продажи </button>
+                    <button class="page-btn" onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'"> Внутренние движения </button>
                 </div>
-                <button onclick="window.location='{{ route('product_moves.purchases_crud') }}'"> Покупки </button>
-                <button onclick="window.location='{{ route('product_moves.sales_crud') }}'"> Продажи </button>
-                <button onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'"> Внутренние движения </button>
+            </div>
 
-                <button onclick="window.location='{{ route('product_moves.general_totals_report') }}'"> Общий отчет </button>
-                <button onclick="window.location='{{ route('product_moves.quantities_report') }}'"> Отчет количеств </button>
+            <div class="card">
+                <div class="card-header">{{ __('Отчеты') }}</div>
+                <div class="card-body vertical-arrange">
+                    <button class="page-btn" onclick="window.location='{{ route('product_moves.general_totals_report') }}'"> Общий отчет </button>
+                    <button class="page-btn" onclick="window.location='{{ route('product_moves.quantities_report') }}'"> Отчет количеств </button>
+                </div>
+            </div>
 
-                <button onclick="window.location='{{ route('products.crud') }}'"> products_crud </button>
-                <button onclick="window.location='{{ route('storages.crud') }}'"> storages_crud </button>
+            <div class="card">
+                <div class="card-header">{{ __('Справочник') }}</div>
+                <div class="card-body vertical-arrange">
+                    <button class="page-btn" onclick="window.location='{{ route('products.crud') }}'"> products_crud </button>
+                    <button class="page-btn" onclick="window.location='{{ route('storages.crud') }}'"> storages_crud </button>
+                </div>
             </div>
         </div>
     </div>
