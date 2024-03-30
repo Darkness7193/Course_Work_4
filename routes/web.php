6bf3b23117bad569940981f73fa687a8b99db84e
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-30 16:13:10 +0300

    0.219 Replace laravel/ui auth by laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StorageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /**
  * @var string $post_to_get_route
  */
 
 
-Route::get('', [HomeController::class, 'index'])->name('home');
+//Route::get('', [HomeController::class, 'index'])->name('home');
 
 
 Route::group(['namespace' => 'App\Http\Controllers\ProductMove'], function() {
@@ -35,3 +36,26 @@ Route::group(['namespace' => 'App\Http\Controllers\TableViewCommands'], function
     Route::post('set_filter', 'SetFilter')->name('set_filter');
     Route::post('set_order', 'SetOrder')->name('set_order');
 });
+
+
+
+
+/*Profiles*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+
+require __DIR__.'/auth.php';
