31381a0c11e183bfd43c09646700d725b90288f1
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-03-30 15:37:33 +0300

    Revert "0.215 Yet home css updates"
    This reverts commit 6ec1b4f814f62c82ee1c10671631e25bc41b3c2a.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8 horizontal-arrange" style="margin-top: 40px">
+        <div class="col-md-8">
 
             <div class="card">
                 <div class="card-header">{{ __('Движения') }}</div>
