b3aba93047ee2f5fffc87f6486ef788544e3e5a2
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-01 22:30:02 +0300

    0.222 Link-button on app header now are refer to view/pages/home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,14 +5,25 @@
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
 
+
 <body>
-    <button onclick="window.location='{{ route('product_moves.purchases_crud') }}'"> Покупки </button>
-    <button onclick="window.location='{{ route('product_moves.sales_crud') }}'"> Продажи </button>
-    <button onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'"> Внутренние движения </button>
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <button onclick="window.location='{{ route('product_moves.purchases_crud') }}'"> Покупки </button>
+                    <button onclick="window.location='{{ route('product_moves.sales_crud') }}'"> Продажи </button>
+                    <button onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'"> Внутренние движения </button>
 
-    <button onclick="window.location='{{ route('product_moves.general_totals_report') }}'"> Общий отчет </button>
-    <button onclick="window.location='{{ route('product_moves.quantities_report') }}'"> Отчет количеств </button>
+                    <button onclick="window.location='{{ route('product_moves.general_totals_report') }}'"> Общий отчет </button>
+                    <button onclick="window.location='{{ route('product_moves.quantities_report') }}'"> Отчет количеств </button>
 
-    <button onclick="window.location='{{ route('products.crud') }}'"> products_crud </button>
-    <button onclick="window.location='{{ route('storages.crud') }}'"> storages_crud </button>
+                    <button onclick="window.location='{{ route('products.crud') }}'"> products_crud </button>
+                    <button onclick="window.location='{{ route('storages.crud') }}'"> storages_crud </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\ProfileController;
  */
 
 
-//Route::get('', [HomeController::class, 'index'])->name('home');
+//
 
 
 Route::group(['namespace' => 'App\Http\Controllers\ProductMove'], function() {
@@ -41,15 +41,9 @@ Route::group(['namespace' => 'App\Http\Controllers\TableViewCommands'], function
 
 
 /*Profiles*/
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('', [HomeController::class, 'index'])->name('home');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
