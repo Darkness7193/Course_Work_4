e8923c8a5ed64e6a7cb24641dde59e0674875cd2
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-01 23:04:38 +0300

    0.225 Middleware auth on the all pages. web.php reorganizing

--- routes/web.php
+++ routes/web.php
@@ -13,37 +13,33 @@ use App\Http\Controllers\ProfileController;
  */
 
 
-//
-
-
-Route::group(['namespace' => 'App\Http\Controllers\ProductMove'], function() {
-    Route::get('product_moves/purchases_crud', 'cruds\PurchasesCrud')->name('product_moves.purchases_crud');
-    Route::get('product_moves/sales_crud', 'cruds\SalesCrud')->name('product_moves.sales_crud');
-    Route::get('product_moves/inner_moves_crud', 'cruds\InnerMovesCrud')->name('product_moves.inner_moves_crud');
-    Route::get('product_moves/general_totals_report', 'reports\GeneralTotalsReport')->name('product_moves.general_totals_report');
-    Route::get('product_moves/quantities_report', 'reports\QuantitiesReport')->name('product_moves.quantities_report');
-});
-
-
-Route::get('products/crud', [ProductController::class, 'index'])->name('products.crud');
-Route::get('storages/crud', [StorageController::class, 'index'])->name('storages.crud');
-
-
-Route::post('post_to_get_route', $post_to_get_route)->name('post_to_get_route');
-Route::group(['namespace' => 'App\Http\Controllers\TableViewCommands'], function() {
-    Route::post('bulk_update_or_create', 'UpdateOrCreateInBulk')->name('bulk_update_or_create');
-    Route::post('bulk_delete', 'DeleteInBulk')->name('bulk_delete');
-    Route::post('set_filter', 'SetFilter')->name('set_filter');
-    Route::post('set_order', 'SetOrder')->name('set_order');
-});
-
-
-
-
-/*Profiles*/
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () use($post_to_get_route) {
+    /*Home*/
     Route::get('', [HomeController::class, 'index'])->name('home');
 
+    /*Cruds & Reports*/
+    Route::group(['namespace' => 'App\Http\Controllers\ProductMove'], function() {
+        Route::get('product_moves/purchases_crud', 'cruds\PurchasesCrud')->name('product_moves.purchases_crud');
+        Route::get('product_moves/sales_crud', 'cruds\SalesCrud')->name('product_moves.sales_crud');
+        Route::get('product_moves/inner_moves_crud', 'cruds\InnerMovesCrud')->name('product_moves.inner_moves_crud');
+        Route::get('product_moves/general_totals_report', 'reports\GeneralTotalsReport')->name('product_moves.general_totals_report');
+        Route::get('product_moves/quantities_report', 'reports\QuantitiesReport')->name('product_moves.quantities_report');
+    });
+
+    /*Catalogs*/
+    Route::get('products/crud', [ProductController::class, 'index'])->name('products.crud');
+    Route::get('storages/crud', [StorageController::class, 'index'])->name('storages.crud');
+
+    /*Commands*/
+    Route::post('post_to_get_route', $post_to_get_route)->name('post_to_get_route');
+    Route::group(['namespace' => 'App\Http\Controllers\TableViewCommands'], function() {
+        Route::post('bulk_update_or_create', 'UpdateOrCreateInBulk')->name('bulk_update_or_create');
+        Route::post('bulk_delete', 'DeleteInBulk')->name('bulk_delete');
+        Route::post('set_filter', 'SetFilter')->name('set_filter');
+        Route::post('set_order', 'SetOrder')->name('set_order');
+    });
+
+    /*Profiles*/
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
