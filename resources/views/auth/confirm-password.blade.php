9e489fd86c207cada827d42853f8725ea638215d
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-01 22:59:39 +0300

    0.224 partially translated on russian views/auth/*, except verify-email.blade.php

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -5,6 +5,7 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+        {{ __('Эта область сайта имеет огранченный доступ. Подтвердите пароль прежде чем продолжить.') }}
     </div>
 
     <form method="POST" action="{{ route('password.confirm') }}">
@@ -12,7 +13,7 @@
 
         <!-- Password -->
         <div>
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Пароль')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -24,7 +25,7 @@
 
         <div class="flex justify-end mt-4">
             <x-primary-button>
-                {{ __('Confirm') }}
+                {{ __('Подтвердить') }}
             </x-primary-button>
         </div>
     </form>
